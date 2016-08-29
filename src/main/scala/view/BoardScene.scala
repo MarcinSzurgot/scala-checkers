ae9e60d678e495a39279b97b468b2579719d6ca7
Mateusz Kozlowski <matikz1110@gmail.com>
2016-08-29 14:34:55 +0200

    Scalafx fixed

--- src/main/scala/view/BoardScene.scala
+++ src/main/scala/view/BoardScene.scala
@@ -2,12 +2,9 @@ package view
 
 import scalafx.scene.layout._
 import scalafx.Includes._
-import scalafx.geometry.{HPos, Insets, VPos}
+import scalafx.geometry.{HPos, VPos}
 import scalafx.scene.Scene
-import scalafx.scene.control.Control
-import scalafx.scene.paint.{LinearGradient, Stops}
-import scalafx.scene.text.Text
-import scalafx.scene.paint.Color._
+
 
 class BoardScene extends Scene
 {
@@ -15,30 +12,16 @@ class BoardScene extends Scene
   final val START_POS = 1;
 
   content = new GridPane {
-    hgap = 8
-    vgap = 8
-    columnConstraints += new ColumnConstraints(5, 10, Double.PositiveInfinity, Priority.ALWAYS, HPos.CENTER, true).delegate
-//      5,
-//      Control.USE_COMPUTED_SIZE,
-//      Double.PositiveInfinity,
-//      ,
-//      HPos.CENTER,
-//      true
-    rowConstraints += new RowConstraints(5, 10, Double.PositiveInfinity, Priority.ALWAYS, VPos.CENTER, true).delegate
-      for (i <- START_POS to BOARD_LENGTH) {
-        for (a <- START_POS to BOARD_LENGTH) {
-          add(new StackPane {
-//            children +=
-//              new Text {
-//                text = "Hello "
-//                style = "-fx-font-size: 48pt"
-//                fill = new LinearGradient(
-//                  endX = 0,
-//                  stops = Stops(PaleGreen, SeaGreen))
-//              }.delegate
-            if (1 == (i+a) % 2) styleClass.add("blackSquare") else styleClass.add("whiteSquare")
-          }.delegate, i, a)
-        }
+    columnConstraints += new ColumnConstraints(5, 60, Double.PositiveInfinity, Priority.ALWAYS, HPos.CENTER, true).delegate
+    rowConstraints += new RowConstraints(5, 60, Double.PositiveInfinity, Priority.ALWAYS, VPos.CENTER, true).delegate
+    for (i <- START_POS to BOARD_LENGTH) {
+      columnConstraints += new ColumnConstraints(5, 60, Double.PositiveInfinity, Priority.ALWAYS, HPos.CENTER, true).delegate
+      rowConstraints += new RowConstraints(5, 60, Double.PositiveInfinity, Priority.ALWAYS, VPos.CENTER, true).delegate
+      for (a <- START_POS to BOARD_LENGTH) {
+        add(new StackPane {
+          if (1 == (i+a) % 2) styleClass += "blackSquare" else styleClass += "whiteSquare"
+        }.delegate, i, a)
       }
+    }
   }
 }
