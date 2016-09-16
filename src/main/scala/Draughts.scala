9082b9bca1637da1540780d411fa8db090b2b0e5
Mateusz Kozlowski <matikz1110@gmail.com>
2016-09-16 22:34:10 +0200

    Scalafx some logic and test

--- src/main/scala/Draughts.scala
+++ src/main/scala/Draughts.scala
@@ -3,20 +3,21 @@ import main.scala.view.BoardScene
 import scalafx.application.{JFXApp, Platform}
 import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.Scene
-import scalafx.scene.control.{Button, Label}
-import scalafx.scene.input.MouseEvent
-import scalafx.scene.layout.{BorderPane, HBox}
+import scalafx.scene.control.{Button, ComboBox, Label}
+import scalafx.scene.layout.{BorderPane, HBox, Priority}
 import scalafx.Includes._
+import scalafx.collections.ObservableBuffer
 import scalafx.stage.Stage
 
 object Draughts extends JFXApp {
   stage = new JFXApp.PrimaryStage {
     title.value = "Draughts"
     width = 300
-    height = 500
+    height = 450
     scene = new Scene {
       stylesheets = List(getClass.getResource("styles.css").toExternalForm)
       content = new BorderPane {
+        styleClass += "root"
         padding = Insets(50)
         top = new Label("Draughts") {
           id = "Title"
@@ -25,6 +26,9 @@ object Draughts extends JFXApp {
           minHeight = 200
         }
         center = new HBox() {
+          vgrow = Priority.Always
+          hgrow = Priority.Always
+          spacing = 15
           children = Seq(
             new Button("Start") {
               onAction = handle {
@@ -44,6 +48,16 @@ object Draughts extends JFXApp {
               minWidth = 100
               onAction = handle {Platform.exit()}
             }
+          )
+          minHeight = 100
+        }
+
+        bottom = new ComboBox[String] {
+          minWidth = 215
+          value = "One Player"
+          items = ObservableBuffer(
+            "One Player",
+            "Two Players"
           )
         }
       }
