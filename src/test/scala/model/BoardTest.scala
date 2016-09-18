43b737ad6496701d5c2bb1ebc59114d528486d5d
bzielinski91 <bzielinski91@wp.pl>
2016-09-18 16:59:48 +0200

    test ruchu pionkow

--- src/test/scala/model/BoardTest.scala
+++ src/test/scala/model/BoardTest.scala
@@ -111,8 +111,106 @@ class BoardTest extends FunSuite with BeforeAndAfterEach {
 
     for (beat <- beats) {
       assert(moves._1.contains(beat._1));
-      assert(moves._2.contains((beat._2, beat._3)));
+      if(beat._2 != null) {
+        assert(moves._2.contains((beat._2, beat._3)));
+      }
     }
+  }
+
+  test("isGameEndTrue"){
+    val modelSt = Array(
+      Array(EMPTY, WHITE, EMPTY, WHITE, EMPTY, WHITE, EMPTY, WHITE),
+      Array(WHITE, EMPTY, WHITE, EMPTY, WHITE, EMPTY, WHITE, EMPTY),
+      Array(EMPTY, EMPTY, EMPTY, WHITE, EMPTY, WHITE, EMPTY, WHITE),
+      Array(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY),
+      Array(EMPTY, WHITE_PROMOTED, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY),
+      Array(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY),
+      Array(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY),
+      Array(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY));
+      var boardTest = Board(PlayerType.WHITE,modelSt);
+      assert(boardTest.isGameEnd()==true)
+  }
+
+  test("isGameEndFalse"){
+    var boardTest = Board(PlayerType.WHITE,modelState);
+    assert(boardTest.isGameEnd()==false)
+  }
+
+  test("isGameEndTrueForBlockedBLACK"){
+    val modelBlocked = Array(
+      Array(EMPTY, WHITE, EMPTY, WHITE, EMPTY, WHITE, EMPTY, WHITE),
+      Array(WHITE, EMPTY, WHITE, EMPTY, WHITE, EMPTY, WHITE, EMPTY),
+      Array(EMPTY, WHITE, EMPTY, WHITE, EMPTY, WHITE, EMPTY, EMPTY),
+      Array(BLACK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, WHITE, EMPTY),
+      Array(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, BLACK),
+      Array(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY),
+      Array(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY),
+      Array(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY));
+    var boardTest = Board(PlayerType.BLACK,modelBlocked);
+    assert(boardTest.isGameEnd()==true)
+  }
+
+  test("isGameEndTrueForBlockedWHITE"){
+    val modelBlockedWhite = Array(
+      Array(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY),
+      Array(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY),
+      Array(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY),
+      Array(WHITE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY),
+      Array(EMPTY, BLACK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, WHITE),
+      Array(BLACK, EMPTY, BLACK, EMPTY, BLACK, EMPTY, BLACK, EMPTY),
+      Array(EMPTY, BLACK, EMPTY, EMPTY, EMPTY, BLACK, EMPTY, BLACK),
+      Array(BLACK, EMPTY, BLACK, EMPTY, BLACK, EMPTY, BLACK, EMPTY));
+    var boardTest = Board(PlayerType.WHITE,modelBlockedWhite);
+    assert(boardTest.isGameEnd()==true)
+  }
+
+  test("isGameEndTrueForBlockedWHITE_PROMOTED"){
+    val modelBlockedWhite = Array(
+      Array(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY),
+      Array(EMPTY, EMPTY, BLACK_PROMOTED, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY),
+      Array(EMPTY, BLACK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY),
+      Array(WHITE_PROMOTED, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY),
+      Array(EMPTY, BLACK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, WHITE),
+      Array(BLACK, EMPTY, BLACK, EMPTY, BLACK, EMPTY, BLACK, EMPTY),
+      Array(EMPTY, BLACK, EMPTY, EMPTY, EMPTY, BLACK, EMPTY, BLACK),
+      Array(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY));
+    var boardTest = Board(PlayerType.WHITE,modelBlockedWhite);
+    assert(boardTest.isGameEnd()==true)
+  }
+
+  test("isGameEndTrueForBlockedBLACK_PROMOTED"){
+    val modelBlocked = Array(
+      Array(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY),
+      Array(WHITE, EMPTY, WHITE, EMPTY, WHITE, EMPTY, WHITE, EMPTY),
+      Array(EMPTY, WHITE, EMPTY, WHITE, EMPTY, WHITE, EMPTY, EMPTY),
+      Array(BLACK_PROMOTED, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, WHITE, EMPTY),
+      Array(EMPTY, WHITE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, BLACK),
+      Array(EMPTY, EMPTY, WHITE, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY),
+      Array(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY),
+      Array(EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY));
+    var boardTest = Board(PlayerType.BLACK,modelBlocked);
+    assert(boardTest.isGameEnd()==true)
+  }
+
+
+  test("makeMoveBeatingForMultipleBeating") {
+    var player = PlayerType.BLACK;
+    var blackPawn1 = Position(0, 3);
+    var blackPawn2 = Position(6, 7);
+    var whitePawn1 = Position(1, 2);
+    var whitePawn2 = Position(1, 4);
+    var whitePawn3 = Position(1, 6);
+    var whitePawn4 = Position(5, 4);
+    var whitePawn5 = Position(5, 6);
+    var whitePawn6 = Position(7, 6);
+    var pawns = List((blackPawn1, BLACK),(blackPawn2, BLACK), (whitePawn1, WHITE),(whitePawn2, WHITE),(whitePawn3, WHITE),(whitePawn4, WHITE),(whitePawn5, WHITE),(whitePawn6, WHITE));
+    var beat1 = (Move(blackPawn1, Position(whitePawn1.row - 1, whitePawn1.col + 1)),whitePawn1,WHITE);
+    var beat2 = (Move(blackPawn1, Position(whitePawn2.row + 1, whitePawn2.col + 1)),whitePawn2,WHITE);
+    var beat3 = (Move(blackPawn2, Position(whitePawn5.row - 1, whitePawn5.col - 1)),whitePawn5,WHITE);
+
+    var beats = List(beat1, beat2, beat3);
+
+    testBeats(player, pawns, beats);
   }
 
 }
